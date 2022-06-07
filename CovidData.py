3957d131f3ec7c01ad22badffa9f2846170ae704
haalbright <67480690+haalbright@users.noreply.github.com>
2022-06-06 20:34:05 -0700

    Add files via upload

--- CovidData.py
+++ CovidData.py
@@ -9,7 +9,7 @@ def graphFrequencies(df, folder):
         val = dfnona[col].value_counts() #counts frequency of all values except NA
         val["NA"] = df[col].isna().sum()  # need to add column for NA manually
         gTitle = col + " Value Frequencies"
-        val.plot(kind="bar", rot=15, title=gTitle, ylabel="Frequency", fontsize=5, figsize=(7.5, 5.5))
+        val.plot(kind="bar", rot=110, title=gTitle, ylabel="Frequency", fontsize=10, figsize=(7.10, 10.10))
         fname = folder+"/" + col + ".png"
         plt.savefig(fname)
 
@@ -35,6 +35,7 @@ xnum=dfx2num.drop("death_yn",axis=1).values #array of x values of the numerical 
 ynum=dfx2num["death_yn"].values  #array of y values (target variable) which is death_yn
 selectnum = SelectKBest(score_func=f_classif, k=1) #since there are only 2 numerical features, we only need k=1 to see which is the better feature
 znum = selectnum.fit_transform(xnum, ynum)
+print(selectnum.scores_)
 colsnum = selectnum.get_support()
 print("K=1", " Features: ", featuresnum.loc[colsnum].values)
 ####################
@@ -49,37 +50,189 @@ for col in ["age_group", "sex", "race", "ethnicity", "process", "exposure_yn", "
     le=LabelEncoder()#label encode categorical variables to prepare for selectkbest
     dfx2.loc[:,col]=le.fit_transform(dfx2.loc[:,col])
 
-features=pd.Series(dfx2.drop(["death_yn"],axis=1).columns)
-x=dfx2.drop(["death_yn"],axis=1).values
-y=dfx2.loc[:,"death_yn"].values
-for kval in range(1,14):
-    select = SelectKBest(chi2, k=kval)
-    z = select.fit_transform(x, y)
-    print(select.scores_)
-    cols = select.get_support()
-    print("K=", kval, " Features: ", features.loc[cols].values)
+# features=pd.Series(dfx2.drop(["death_yn"],axis=1).columns)
+# x=dfx2.drop(["death_yn"],axis=1).values
+# y=dfx2.loc[:,"death_yn"].values
+# for kval in range(1,14):
+#     select = SelectKBest(chi2, k=kval)
+#     z = select.fit_transform(x, y)
+#     print(select.scores_)
+#     cols = select.get_support()
+#     print("K=", kval, " Features: ", features.loc[cols].values)
 ##########################
 #graphFrequencies(df2, "Column_counts")
 df["sex"].replace("Unknown", "Other/Unknown", inplace=True)
-print(df["sex"].drop_duplicates())
-print("Exposure: ", df["exposure_yn"].drop_duplicates())
-df3=df.drop(["case_month","county_fips_code", "ethnicity", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+# print(df["sex"].drop_duplicates())
+# print("Exposure: ", df["exposure_yn"].drop_duplicates())
+# df3=df.drop(["case_month","county_fips_code", "ethnicity", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+# print("Before: ", df3.shape)
+# df3.dropna(inplace=True)
+# print("Exposure: ", df3["exposure_yn"].drop_duplicates())
+# df3bool=~df3.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# df3b=df3bool.all(axis=1)#returns bool series (True if the row has all true values)
+# df3after=df3.loc[df3b]
+# print("Exposure: ", df3after["exposure_yn"].drop_duplicates())
+# print("After: ", df3after.shape)
+# df3after.to_csv("CovData/df3.csv", index=False)
+# ['icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process', 'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+df.drop("state_fips_code","county_fips_code","case_month",)
+df1=df.drop(["underlying_conditions_yn", "ethnicity", "sex","symptom_status", "race", "current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df1.shape)
+df1.dropna(inplace=True)
+df1bool=~df1.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df1b=df1bool.all(axis=1)#returns bool series (True if the row has all true values)
+df1after=df1.loc[df1b]
+print("After: ", df1after.shape)
+df1after.to_csv("CovData/df1.csv", index=False)
+
+
+df2=df.drop(["ethnicity",  "sex","symptom_status", "race", "current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df2.shape)
+df2.dropna(inplace=True)
+df2bool=~df2.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df2b=df2bool.all(axis=1)#returns bool series (True if the row has all true values)
+df2after=df2.loc[df2b]
+print("After: ", df2after.shape)
+df2after.to_csv("CovData/df2.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+df3=df.drop(["sex","symptom_status", "race", "current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
 print("Before: ", df3.shape)
 df3.dropna(inplace=True)
-print("Exposure: ", df3["exposure_yn"].drop_duplicates())
 df3bool=~df3.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
 df3b=df3bool.all(axis=1)#returns bool series (True if the row has all true values)
 df3after=df3.loc[df3b]
-print("Exposure: ", df3after["exposure_yn"].drop_duplicates())
 print("After: ", df3after.shape)
-df3after.to_csv("CovData/df3.csv")
+df3after.to_csv("CovData/df3.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
 
-df4=df.drop(["case_month","county_fips_code", "ethnicity", "case_positive_specimen_interval","underlying_conditions_yn", "case_onset_interval"], axis=1) #columns to definitely drop
+df4=df.drop(["symptom_status", "race", "current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
 print("Before: ", df4.shape)
 df4.dropna(inplace=True)
 df4bool=~df4.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
 df4b=df4bool.all(axis=1)#returns bool series (True if the row has all true values)
 df4after=df4.loc[df4b]
-print("Exposure4: ", df4after["exposure_yn"].drop_duplicates())
 print("After: ", df4after.shape)
-df4after.to_csv("CovData/df4.csv")
+df4after.to_csv("CovData/df4.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+df5=df.drop([ "race", "current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df5.shape)
+df5.dropna(inplace=True)
+df5bool=~df5.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df5b=df5bool.all(axis=1)#returns bool series (True if the row has all true values)
+df5after=df5.loc[df5b]
+print("After: ", df5after.shape)
+df5after.to_csv("CovData/df5.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+df6=df.drop(["current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df6.shape)
+df6.dropna(inplace=True)
+df6bool=~df6.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df6b=df6bool.all(axis=1)#returns bool series (True if the row has all true values)
+df6after=df6.loc[df6b]
+print("After: ", df6after.shape)
+df6after.to_csv("CovData/df6.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+df7=df.drop([ "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df7.shape)
+df7.dropna(inplace=True)
+df7bool=~df7.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df7b=df7bool.all(axis=1)#returns bool series (True if the row has all true values)
+df7after=df7.loc[df7b]
+print("After: ", df7after.shape)
+df7after.to_csv("CovData/df7.csv", index=False)
+#'icu_yn', 'hosp_yn', 'age_group,'county_fips_code', 'process',  'underlying_conditions_yn', 'ethnicity,  'sex', 'symptom_status, 'race', 'current_status', ‘'state_fips_code', 'exposure_yn'
+
+# df8=df.drop(["exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+# print("Before: ", df8.shape)
+# df8.dropna(inplace=True)
+# df8bool=~df8.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# df8b=df8bool.all(axis=1)#returns bool series (True if the row has all true values)
+# df8after=df8.loc[df8b]
+# print("After: ", df8after.shape)
+# df8after.to_csv("CovData/df8.csv", index=False)
+
+# df7=df.drop([ "ethnicity", "case_positive_specimen_interval", "case_onset_interval", ], axis=1) #columns to definitely drop
+# print("Before: ", df7.shape)
+# df7.dropna(inplace=True)
+# df7bool=~df7.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# df7b=df7bool.all(axis=1)#returns bool series (True if the row has all true values)
+# df7after=df7.loc[df7b]
+# print("After: ", df7after.shape)
+# df7after.to_csv("CovData/df7.csv", index=False)
+
+# "ethnicity", "case_positive_specimen_interval", "case_onset_interval",
+df9=df.drop(["current_status", "exposure_yn","case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+print("Before: ", df9.shape)
+df9.dropna(inplace=True)
+df9bool=~df9.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+df9b=df9bool.all(axis=1)#returns bool series (True if the row has all true values)
+df9after=df9.loc[df9b]
+print("After: ", df9after.shape)
+df9after.to_csv("CovData/df9.csv", index=False)
+####this df is for the tree. It does its own feature selection, however I am deleting columns with large NA counts
+
+
+
+
+
+
+
+####DONT DELETE
+# df10=df.drop(["case_onset_interval", "case_positive_specimen_interval"], axis=1) #columns to definitely drop
+# print("Before: ", df10.shape)
+# df10.dropna(inplace=True)
+# df10bool=~df10.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# df10b=df10bool.all(axis=1)#returns bool series (True if the row has all true values)
+# df10after=df10.loc[df10b]
+# print("After: ", df10after.shape)
+# df10after.to_csv("CovData/df10.csv", index=False)
+#
+# dftree1=df.drop(["case_onset_interval", "case_positive_specimen_interval", "underlying_conditions_yn"], axis=1) #columns to definitely drop
+# print("Before: ", dftree1.shape)
+# dftree1.dropna(inplace=True)
+# dftree1bool=~dftree1.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# dftree1b=dftree1bool.all(axis=1)#returns bool series (True if the row has all true values)
+# dftree1after=dftree1.loc[dftree1b]
+# print("After: ", dftree1after.shape)
+# dftree1after.to_csv("CovData/dftree1.csv", index=False)
+#
+# dftree2=df.drop(["case_onset_interval", "case_positive_specimen_interval", "ethnicity", "underlying_conditions_yn"], axis=1) #columns to definitely drop
+# print("Before: ", dftree2.shape)
+# dftree2.dropna(inplace=True)
+# dftree2bool=~dftree2.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# dftree2b=dftree2bool.all(axis=1)#returns bool series (True if the row has all true values)
+# dftree2after=dftree2.loc[dftree2b]
+# print("After: ", dftree2after.shape)
+# dftree2after.to_csv("CovData/dftree2.csv", index=False)
+#
+# dftree3=df.drop(["case_onset_interval", "case_positive_specimen_interval", "ethnicity", "underlying_conditions_yn", "exposure_yn"], axis=1) #columns to definitely drop
+# print("Before: ", dftree3.shape)
+# dftree3.dropna(inplace=True)
+# dftree3bool=~dftree3.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# dftree3b=dftree3bool.all(axis=1)#returns bool series (True if the row has all true values)
+# dftree3after=dftree3.loc[dftree3b]
+# print("After: ", dftree3after.shape)
+# dftree3after.to_csv("CovData/dftree3.csv", index=False)
+#
+# dftree4=df.drop(["case_onset_interval", "case_positive_specimen_interval", "process", "underlying_conditions_yn", "exposure_yn"], axis=1) #columns to definitely drop
+# print("Before: ", dftree4.shape)
+# dftree4.dropna(inplace=True)
+# dftree4bool=~dftree4.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# dftree4b=dftree4bool.all(axis=1)#returns bool series (True if the row has all true values)
+# dftree4after=dftree4.loc[dftree4b]
+# print("After: ", dftree4after.shape)
+# dftree4after.to_csv("CovData/dftree4.csv", index=False)
+#
+# dftree5=df.drop(["case_onset_interval", "case_positive_specimen_interval", "process", "underlying_conditions_yn", "exposure_yn", "ethnicity"], axis=1) #columns to definitely drop
+# print("Before: ", dftree5.shape)
+# dftree5.dropna(inplace=True)
+# dftree5bool=~dftree5.isin(["Unknown", "Missing"])#returns bool dataframe (false if unknown or missing)
+# dftree5b=dftree5bool.all(axis=1)#returns bool series (True if the row has all true values)
+# dftree5after=dftree5.loc[dftree5b]
+# print("After: ", dftree5after.shape)
+# dftree5after.to_csv("CovData/dftree5.csv", index=False)
---
+++ CovidModels.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn import metrics, preprocessing, tree
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, CategoricalNB
+import numpy as np
+
+def CNB(xTrain, xTest, yTrain, yTest):
+    model = CategoricalNB()
+    model.fit(xTrain, yTrain)
+    print(xTest.shape)
+    yPred = model.predict(xTest)
+    print("Precision:", metrics.precision_score(yTest, yPred))
+    print("Recall:", metrics.recall_score(yTest, yPred))
+    print("F1:", metrics.f1_score(yTest, yPred))
+    print("Test Accuracy: ", metrics.accuracy_score(yTest, yPred))
+    return np.concatenate((yTest, yPred), axis=0)
+
+for filenum in [1,2,3,4,5,6,7,8,9]:
+    dset="CovData/df" + str(filenum)+".csv"
+    df=pd.read_csv(dset)
+    print("\n", dset, " Shape: ", df.shape)
+    if "county_fips_code" in df.columns:
+        df = df.drop(["county_fips_code"], axis=1)
+    x=df.drop(["death_yn"], axis=1)
+    x=x.astype("category")
+    x2 = pd.get_dummies(x) # county code wont do OHE!
+    xOHE = x2.to_numpy()
+    y = df["death_yn"].values
+    le = preprocessing.LabelEncoder()
+    yLE = le.fit_transform(y)
+    x_train, x_test, y_train, y_test = train_test_split(xOHE, yLE, test_size=0.30, random_state=123, stratify=yLE) #using stratify since its unbalanced
+    result=CNB(x_train, x_test, y_train, y_test)
